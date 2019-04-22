1414fa1d2145e387dd843836ddf8dc13eaa30168
Tyler Impey <tylerimpey@me.com>
2019-04-22 15:25:06 -0400

    Update leetcode_examples.py

--- leetcode_examples.py
+++ leetcode_examples.py
@@ -512,6 +512,39 @@ class EasySolutions(object):
 
         return n>>2<<2 != n
 
+    def licenseKeyFormatting_482(self, S, K):
+        """
+        Description
+        -----------
+        You are given a license key represented as a string S which consists only alphanumeric character and dashes. The string is separated into N+1 groups by N dashes. Given a number K, we would want to reformat the strings such that each group contains exactly K characters except for the first group which could be shorter than K, but still must contain at least one character. Furthermore, there must be a dash inserted between two groups and all lowercase letters should be converted to uppercase. Given a non-empty string S and a number K format the string according to the rules described above.
+        
+        Parameters
+        ----------
+        :type S:    str
+        :type K:    int
+        :rtype:     str
+        """
+        
+        S = S.replace('-','')[::-1].upper()
+        
+        groups = int(len(S)/K)
+        
+        result = ''
+        for i in range(groups):
+            result += S[i*K:i*K+K] + '-'
+            
+        result += S[groups*K:]
+        
+        result = result[::-1]
+        
+        if len(result) == 0:
+            return ''
+        
+        if result[0] == '-':
+            result = result[1:]
+        
+        return result
+
     def numJewelsInStones_771(self, J, S):
         """
         Description
