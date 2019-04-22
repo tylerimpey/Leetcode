25112fc20bbade2f46cdbd1ddd9c87b6f1241ec3
Tyler Impey <tylerimpey@me.com>
2019-04-22 15:17:32 -0400

    Update leetcode_examples.py

--- leetcode_examples.py
+++ leetcode_examples.py
@@ -508,7 +508,9 @@ class EasySolutions(object):
         :rtype:     bool
         """
         
-        return n % 4 != 0
+        # return n % 4 != 0
+
+        return n>>2<<2 != n
 
     def numJewelsInStones_771(self, J, S):
         """
@@ -543,6 +545,32 @@ class EasySolutions(object):
             jewels += sum([1 for j in S if j == i])
 
         return jewels
+
+    def numUniqueEmails_929(self, emails):
+        """
+        Description
+        -----------
+        Every email consists of a local name and a domain name, separated by the @ sign. For example, in alice@leetcode.com, alice is the local name, and leetcode.com is the domain name. Besides lowercase letters, these emails may contain '.'s or '+'s. If you add periods ('.' between some characters in the local name part of an email address, mail sent there will be forwarded to the same address without dots in the local name.  For example, "alice.z@leetcode.com" and "alicez@leetcode.com" forward to the same email address. (Note that this rule does not apply for domain names.) If you add a plus ('+') in the local name, everything after the first plus sign will be ignored. This allows certain emails to be filtered, for example m.y+name@email.com will be forwarded to my@email.com. (Again, this rule does not apply for domain names.) It is possible to use both of these rules at the same time. Given a list of emails, we send one email to each address in the list.  How many different addresses actually receive mails?
+
+        Company
+        -------
+        Google
+        
+        Parameters
+        ----------
+        :type emails:   List[str]
+        :rtype:         int
+        """
+        
+        ends   = [email.split('@')[1] for email in emails]
+        starts = [email.split('@')[0] for email in emails]
+        
+        starts = [email.split('+')[0] if '+' in email else email for email in starts]
+        starts = [email.replace('.','') for email in starts]
+        
+        emails = list(set([starts[i] + '@' + ends[i] for i in range(len(emails))]))
+        
+        return len(emails)
 
 
 class MediumSolutions(object):
