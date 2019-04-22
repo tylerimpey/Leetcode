bfcdd6756e8464d18408e5cb74c0293e41559bfb
Tyler Impey <tylerimpey@me.com>
2019-04-22 09:55:11 -0400

    Update leetcode_examples.py

--- leetcode_examples.py
+++ leetcode_examples.py
@@ -314,6 +314,20 @@ class EasySolutions(object):
                 
         return int(mid)
 
+    def singleNumber_136(self, nums):
+        """
+        Description
+        -----------
+        Given a non-empty array of integers, every element appears twice except for one. Find that single one. Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+        
+        Parameters
+        ----------
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        return sum(list(set(nums)))*2 - sum(nums)
+
     def numJewelsInStones_771(self, J, S):
         """
         Description
