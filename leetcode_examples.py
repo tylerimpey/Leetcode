79fc363e7c73631eee8f03061f3509856ac673fe
Tyler Impey <tylerimpey@me.com>
2019-04-22 19:55:51 -0400

    Update leetcode_examples.py

--- leetcode_examples.py
+++ leetcode_examples.py
@@ -426,6 +426,24 @@ class EasySolutions(object):
                 
         return int(mid)
 
+    def climbStairs_70(self, n):
+        """
+        Description
+        -----------
+        You are climbing a stair case. It takes n steps to reach to the top. Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top? Note: Given n will be a positive integer.
+        
+        Parameters
+        ----------
+        :type n:    int
+        :rtype:     int
+        """
+        
+        fib = tmp = 1
+        for _ in range(n):
+            fib, tmp = tmp, fib + tmp
+        
+        return fib
+
     def singleNumber_136(self, nums):
         """
         Description
