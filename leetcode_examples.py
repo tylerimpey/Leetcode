fcfccc35f42bb855539072fe26affae5d20ef2b6
Tyler Impey <tylerimpey@me.com>
2019-04-23 18:58:35 -0400

    Update leetcode_examples.py

--- leetcode_examples.py
+++ leetcode_examples.py
@@ -1178,6 +1178,26 @@ class EasySolutions(object):
         
         return len(emails)
 
+    def minDeletionSize_944(self, A):
+        """
+        Description
+        -----------
+        We are given an array A of N lowercase letter strings, all of the same length. Now, we may choose any set of deletion indices, and for each string, we delete all the characters in those indices. For example, if we have an array A = ["abcdef","uvwxyz"] and deletion indices {0, 2, 3}, then the final array after deletions is ["bef", "vyz"], and the remaining columns of A are ["b","v"], ["e","y"], and ["f","z"].  (Formally, the c-th column is [A[0][c], A[1][c], ..., A[A.length-1][c]].) Suppose we chose a set of deletion indices D such that after deletions, each remaining column in A is in non-decreasing sorted order. Return the minimum possible value of D.length.
+        
+        Parameters
+        ----------
+        :type A:    List[str]
+        :rtype:     int
+        """
+        
+        check = 0
+        for i in range(len(A[0])):
+            stack = [ord(x[i]) for x in A]
+            if stack != sorted(stack):
+                check += 1
+        
+        return check
+
     def repeatedNTimes_961(self, A):
         """
         Description
