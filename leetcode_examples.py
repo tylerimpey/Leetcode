16fdfbce5fd3d56af566f45f4e7838e22db2912a
Tyler Impey <tylerimpey@me.com>
2019-04-23 14:45:58 -0400

    Update leetcode_examples.py

--- leetcode_examples.py
+++ leetcode_examples.py
@@ -444,6 +444,23 @@ class EasySolutions(object):
         
         return fib
 
+    def maxProfit_121(self, prices):
+        """
+        Description
+        -----------
+        Say you have an array for which the ith element is the price of a given stock on day i. If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit. Note that you cannot sell a stock before you buy one.
+        
+        Parameters
+        ----------
+        :type prices:   List[int]
+        :rtype:         int
+        """
+        
+        result, buy = 0, float('inf')
+        for p in prices:
+            result, buy = max(result, p-buy), min(buy, p)
+        return result
+
     def singleNumber_136(self, nums):
         """
         Description
