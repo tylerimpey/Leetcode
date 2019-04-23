600683014469b8f9eb687c5b0a946942e59cb419
Tyler Impey <tylerimpey@me.com>
2019-04-23 09:20:30 -0400

    Update leetcode_examples.py

--- leetcode_examples.py
+++ leetcode_examples.py
@@ -779,6 +779,32 @@ class EasySolutions(object):
 
         return list(set(nums1) & set(nums2))
 
+    def fizzBuzz_412(self, n):
+        """
+        Description
+        -----------
+        Write a program that outputs the string representation of numbers from 1 to n. But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+        
+        Parameters
+        ----------
+        :type n:    int
+        :rtype:     List[str]
+        """
+        
+        result = []
+        
+        for i in range(1, n+1):
+            temp = ''
+            if i % 3 == 0:
+                temp += 'Fizz'
+            if i % 5 == 0:
+                temp += 'Buzz'
+            if i % 5 != 0 and i % 3 != 0:
+                temp = str(i)
+            result.append(temp)
+            
+        return result
+
     def licenseKeyFormatting_482(self, S, K):
         """
         Description
