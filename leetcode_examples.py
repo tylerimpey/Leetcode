4549ed345a4a6d8309cb553449ee6a6686d3e24e
Tyler Impey <tylerimpey@me.com>
2019-04-23 16:46:13 -0400

    Update leetcode_examples.py

--- leetcode_examples.py
+++ leetcode_examples.py
@@ -873,6 +873,24 @@ class EasySolutions(object):
         
         return result
 
+    def judgeCircle_657(self, moves):
+        """
+        Description
+        -----------
+        There is a robot starting at position (0, 0), the origin, on a 2D plane. Given a sequence of its moves, judge if this robot ends up at (0, 0) after it completes its moves. The move sequence is represented by a string, and the character moves[i] represents its ith move. Valid moves are R (right), L (left), U (up), and D (down). If the robot returns to the origin after it finishes all of its moves, return true. Otherwise, return false. Note: The way that the robot is "facing" is irrelevant. "R" will always make the robot move to the right once, "L" will always make it move left, etc. Also, assume that the magnitude of the robot's movement is the same for each move.
+        
+        Parameters
+        ----------
+        :type moves:    str
+        :rtype:         bool
+        """
+        
+        directions = {"U": 1, "D": -1, "L": 1j, "R": -1j}
+        
+        movement = sum([directions[i] for i in moves])
+        
+        return not movement
+
     def toLowerCase_709(self, str):
         """
         Description
