bacb2a3d0e0ebba0518fbb242eadd01c745c45f4
Tyler Impey <tylerimpey@me.com>
2019-04-23 21:06:47 -0400

    Update leetcode_examples.py

--- leetcode_examples.py
+++ leetcode_examples.py
@@ -1425,6 +1425,29 @@ class MediumSolutions(object):
             l, r = max(((l, r), expand(i,i), expand(i,i+1)), key=lambda x: x[1]-x[0])
         return s[l:r]
 
+    def maxArea_11(self, height):
+        """
+        Description
+        -----------
+        Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water. Note: You may not slant the container and n is at least 2.
+        
+        Parameters
+        ----------
+        :type height:   List[int]
+        :rtype:         int
+        """
+
+        left = result = 0
+        rite = width = len(height) - 1
+        for w in range(width, 0, -1):
+            if height[left] < height[rite]:
+                result = max(result, height[left]*w)
+                left += 1
+            else:
+                result = max(result, height[rite]*w)
+                rite -= 1
+        return result
+
     def permute_46(self, nums):
         """
         Description
