8cbab1b6ba8c6a0760d767caa0f629423d5bfdd8
Tyler Impey <tylerimpey@me.com>
2019-04-23 21:25:17 -0400

    Update leetcode_examples.py

--- leetcode_examples.py
+++ leetcode_examples.py
@@ -1448,6 +1448,26 @@ class MediumSolutions(object):
                 rite -= 1
         return result
 
+    def intToRoman_12(self, num):
+        """
+        Description
+        -----------
+        For example, two is written as II in Roman numeral, just two one's added together. Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII, which is XX + V + II. Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used: I can be placed before V (5) and X (10) to make 4 and 9. X can be placed before L (50) and C (100) to make 40 and 90. C can be placed before D (500) and M (1000) to make 400 and 900. Given an integer, convert it to a roman numeral. Input is guaranteed to be within the range from 1 to 3999.
+        
+        Parameters
+        ----------
+        :type num:  int
+        :rtype:     str
+        """
+        
+        romans = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        result = ""
+        for i, val in enumerate(values):
+            result += (num//val) * romans[i]
+            num %= val
+        return result
+
     def permute_46(self, nums):
         """
         Description
