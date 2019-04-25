8874ea388c5b5ad857192dfd767eb730fa296e85
Tyler Impey <tylerimpey@me.com>
2019-04-25 15:11:47 -0400

    Update leetcode_examples.py

--- leetcode_examples.py
+++ leetcode_examples.py
@@ -1785,6 +1785,20 @@ class EasySolutions(object):
                 
         return result
 
+    def divisorGame_1025(self, N):
+        """
+        Description
+        -----------
+        Alice and Bob take turns playing a game, with Alice starting first. Initially, there is a number N on the chalkboard.  On each player's turn, that player makes a move consisting of: Choosing any x with 0 < x < N and N % x == 0. Replacing the number N on the chalkboard with N - x. Also, if a player cannot make a move, they lose the game. Return True if and only if Alice wins the game, assuming both players play optimally.
+        
+        Parameters
+        ----------
+        :type N:    int
+        :rtype:     bool
+        """
+        
+        return True if N % 2 == 0 else False
+
 
 class MediumSolutions(object):
     """
