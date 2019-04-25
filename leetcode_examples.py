d1b469291f71e8f72853c60bb030534c6a2d01ce
Tyler Impey <tylerimpey@me.com>
2019-04-25 08:16:38 -0400

    Update leetcode_examples.py

--- leetcode_examples.py
+++ leetcode_examples.py
@@ -1382,6 +1382,36 @@ class EasySolutions(object):
                 
         return result
 
+    def nextGreatestLetter_744(self, letters, target):
+        """
+        Description
+        -----------
+        Given a list of sorted characters letters containing only lowercase letters, and given a target letter target, find the smallest element in the list that is larger than the given target. Letters also wrap around. For example, if the target is target = 'z' and letters = ['a', 'b'], the answer is 'a'.
+        
+        Parameters
+        ----------
+        :type letters:  List[str]
+        :type target:   str
+        :rtype:         str
+        """
+        
+        if len(letters) < 1 or target == None: 
+            return None
+        
+        left = 0
+        rite = len(letters)
+        while left < rite:
+            mid = (left + rite)//2
+            if target >= letters[mid]:
+                left = mid + 1
+            else:
+                rite = mid
+                
+        if left == len(letters): 
+            return letters[0]
+        else: 
+            return letters[left]
+
     def anagramMappings_760(self, A, B):
         """
         Description
