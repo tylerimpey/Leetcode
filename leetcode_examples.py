5d6997e4ca6db3d87c0ea550555f6811f9a6e040
Tyler Impey <tylerimpey@me.com>
2019-04-25 00:26:00 -0400

    Update leetcode_examples.py

--- leetcode_examples.py
+++ leetcode_examples.py
@@ -1241,6 +1241,20 @@ class EasySolutions(object):
             
         return fib[1]
 
+    def reverseWords_557(self, s):
+        """
+        Description
+        -----------
+        Given a string, you need to reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order. Note: In the string, each word is separated by single space and there will not be any extra space in the string.
+        
+        Parameters
+        ----------
+        :type s:    str
+        :rtype:     str
+        """
+        
+        return ' '.join([w[::-1] for w in s.split(' ')])
+
     def arrayPairSum_561(self, nums):
         """
         Description
