a7873913eba8368260df3a86ec7937507ba0accf
Tyler Impey <tylerimpey@me.com>
2019-04-24 23:48:41 -0400

    Update leetcode_examples.py

--- leetcode_examples.py
+++ leetcode_examples.py
@@ -1823,6 +1823,36 @@ class MediumSolutions(object):
                 out += i,
         return out
 
+    def setZeroes_73(self, matrix):
+        """
+        Description
+        -----------
+        Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in-place.
+        
+        Parameters
+        ----------
+        :type matrix:   List[List[int]]
+        :rtype:         None Do not return anything, modify matrix in-place instead.
+        """
+        
+        m = len(matrix)
+        n = len(matrix[0])
+        
+        rows = []
+        cols = []
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    rows += [i]
+                    cols += [j]
+                    
+        for i in rows:
+            matrix[i] = [0]*n
+            
+        for j in cols:
+            for i in range(m):
+                matrix[i][j] = 0
+
     def searchMatrix_74(self, matrix, target):
         """
         :type matrix: List[List[int]]
