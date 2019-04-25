fd69eb2d7f9bb45b268a4bf04805586d21329065
Tyler Impey <tylerimpey@me.com>
2019-04-25 08:33:23 -0400

    Update leetcode_examples.py

--- leetcode_examples.py
+++ leetcode_examples.py
@@ -1588,6 +1588,34 @@ class EasySolutions(object):
 
         return [num for num in A if num % 2 == 0] + [num for num in A if num % 2 == 1]
 
+    def sortArrayByParityII_922(self, A):
+        """
+        Description
+        -----------
+        Given an array A of non-negative integers, half of the integers in A are odd, and half of the integers are even. Sort the array so that whenever A[i] is odd, i is odd; and whenever A[i] is even, i is even. You may return any answer array that satisfies this condition.
+        
+        Parameters
+        ----------
+        :type A:    List[int]
+        :rtype:     List[int]
+        """
+        
+        evn = 0
+        odd = 1
+        l = len(A)
+        
+        while evn < l and odd < l:
+            if A[evn] % 2 == 0:
+                evn += 2
+            elif A[odd] % 2 == 1:
+                odd += 2
+            else: 
+                A[evn], A[odd] = A[odd], A[evn]
+                evn += 2
+                odd += 2
+        
+        return A
+
     def numUniqueEmails_929(self, emails):
         """
         Description
