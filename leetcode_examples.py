82e6c2acdf905d570550f1c86bb6a0a51e3c8287
Tyler Impey <tylerimpey@me.com>
2019-04-25 08:56:22 -0400

    Update leetcode_examples.py

--- leetcode_examples.py
+++ leetcode_examples.py
@@ -2202,5 +2202,12 @@ class SQLSolutions(object):
     All answers are simply described by commented chunks.
     """
 
-    
+    def bigCountries_595(self):
 
+        # SELECT name, population, area
+        # FROM World
+        # WHERE area > 3000000 OR population > 25000000
+
+        return 0
+
+    
