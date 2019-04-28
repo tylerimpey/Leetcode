674ac00246d907886827473d96c05a89f8f06674
Tyler Impey <tylerimpey@me.com>
2019-04-28 13:40:26 -0400

    Update leetcode_examples.py

--- leetcode_examples.py
+++ leetcode_examples.py
@@ -307,6 +307,30 @@ class EasySolutions(object):
             
         return len(nums)
 
+    def myPow_50(self, x, n):
+        """
+        Description
+        -----------
+        Implement pow(x, n), which calculates x raised to the power n (xn).
+        
+        Parameters
+        ----------
+        :type x:    float
+        :type n:    int
+        :rtype:     float
+        """
+        
+        if not n:
+            return 1
+        
+        if n < 0:
+            return 1 / self.myPow(x, -n)
+        
+        if n % 2:
+            return x * self.myPow(x, n-1)
+        
+        return self.myPow(x*x, n/2)
+
     def maxSubArray_53(self, nums):
         """
         Description
